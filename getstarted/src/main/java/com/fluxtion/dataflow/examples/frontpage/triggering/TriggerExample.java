1758020ac64ec26bff9a3b4b8d2b920a80f841d6
greg higgins <gregphiggins@gmail.com>
2025-02-04 14:04:14 +0000

    add trigger example

---
+++ getstarted/src/main/java/com/fluxtion/dataflow/examples/frontpage/triggering/TriggerExample.java
@@ -0,0 +1,32 @@
+package com.fluxtion.dataflow.examples.frontpage.triggering;
+
+import com.fluxtion.dataflow.builder.DataFlowBuilder;
+import com.fluxtion.dataflow.runtime.DataFlow;
+import com.fluxtion.dataflow.runtime.flowfunction.helpers.Aggregates;
+
+public class TriggerExample {
+    public static void main(String[] args) {
+        DataFlow sumDataFlow = DataFlowBuilder.subscribe(Integer.class)
+                .aggregate(Aggregates.intSumFactory())
+                .resetTrigger(DataFlowBuilder.subscribeToSignal("resetTrigger"))
+                .filter(i -> i != 0)
+                .publishTriggerOverride(DataFlowBuilder.subscribeToSignal("publishSumTrigger"))
+                .console("Current sun:{}")
+                .build();
+
+        sumDataFlow.onEvent(10);
+        sumDataFlow.onEvent(50);
+        sumDataFlow.onEvent(32);
+        //publish
+        sumDataFlow.publishSignal("publishSumTrigger");
+
+        //reset sum
+        sumDataFlow.publishSignal("resetTrigger");
+
+        //new sum
+        sumDataFlow.onEvent(8);
+        sumDataFlow.onEvent(17);
+        //publish
+        sumDataFlow.publishSignal("publishSumTrigger");
+    }
+}
