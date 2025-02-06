32ca8a926cd8872916593aaac4fd6e90f045c1e5
greg <gregphiggins@gmail.com>
2025-02-06 12:13:02 +0000

    add jbang directives for MultiFeedJoinExample

--- getstarted/src/main/java/com/fluxtion/dataflow/examples/frontpage/multijoin/MultiFeedJoinExample.java
+++ getstarted/src/main/java/com/fluxtion/dataflow/examples/frontpage/multijoin/MultiFeedJoinExample.java
@@ -3,9 +3,17 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
-//REPOS repsy-fluxtion=https://repo.repsy.io/mvn/fluxtion/fluxtion-public
+//REPOS mavencentral,repsy-fluxtion=https://repo.repsy.io/mvn/fluxtion/fluxtion-public
 //DEPS com.fluxtion.dataflow:dataflow-builder:1.0.0
+//DEPS org.projectlombok:lombok:1.18.36
 //COMPILE_OPTIONS -proc:full
+//SOURCES userfunc/Helpers.java
+//SOURCES userfunc/AlarmDeltaFilter.java
+//SOURCES userfunc/LocationCode.java
+//SOURCES userfunc/MachineProfileEvent.java
+//SOURCES userfunc/MachineReadingEvent.java
+//SOURCES userfunc/MachineState.java
+//SOURCES userfunc/SupportContactEvent.java
 package com.fluxtion.dataflow.examples.frontpage.multijoin;
 
 import com.fluxtion.dataflow.builder.DataFlowBuilder;
@@ -42,6 +50,35 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * A sink is available for the host application to consume the alarm output, in this case a pretty print consumer<br>
  * <br>
+ * Running the app should produce an output similar to below:
+ *
+ * <pre>
+ *  Application started - wait four seconds for first machine readings
+ *
+ *  ALARM UPDATE 14:31:30.785
+ *  New alarms: ['server_GOOG@USA_EAST_1',  temp:'49.16', avgTemp:'52.05' SupportContact[name=Jean, locationCode=USA_EAST_1, contactDetails=jean@fluxtion.com], 'server_TKM@USA_EAST_2',  temp:'86.47', avgTemp:'52.37' SupportContact[name=Tandy, locationCode=USA_EAST_2, contactDetails=tandy@fluxtion.com], 'server_AMZN@USA_EAST_1',  temp:'71.48', avgTemp:'54.25' SupportContact[name=Jean, locationCode=USA_EAST_1, contactDetails=jean@fluxtion.com], 'server_MSFT@USA_EAST_2',  temp:'31.70', avgTemp:'52.53' SupportContact[name=Tandy, locationCode=USA_EAST_2, contactDetails=tandy@fluxtion.com]]
+ *  Alarms to clear[]
+ *  Current alarms[server_GOOG, server_TKM, server_AMZN, server_MSFT]
+ *  ------------------------------------
+ *
+ *  ALARM UPDATE 14:31:32.778
+ *  New alarms: []
+ *  Alarms to clear[server_TKM]
+ *  Current alarms[server_GOOG, server_AMZN, server_MSFT]
+ *  ------------------------------------
+ *
+ *  ALARM UPDATE 14:31:33.768
+ *  New alarms: ['server_TKM@USA_EAST_2',  temp:'98.33', avgTemp:'49.95' SupportContact[name=Tandy, locationCode=USA_EAST_2, contactDetails=tandy@fluxtion.com]]
+ *  Alarms to clear[]
+ *  Current alarms[server_GOOG, server_TKM, server_AMZN, server_MSFT]
+ *  ------------------------------------
+ *
+ *  ALARM UPDATE 14:31:37.777
+ *  New alarms: []
+ *  Alarms to clear[server_AMZN]
+ *  Current alarms[server_GOOG, server_TKM, server_MSFT]
+ *  ------------------------------------
+ * </pre>
  **/
 
 public class MultiFeedJoinExample {
