808c4af82befa60299d04ecaf7b1a39d31371ce0
greg higgins <greg.higgins@v12technology.com>
2025-02-23 19:36:48 +0000

    update DataFlowRunnerSample

--- reference/src/main/java/com/fluxtion/dataflow/reference/eventfeed/DataFlowRunnerSample.java
+++ reference/src/main/java/com/fluxtion/dataflow/reference/eventfeed/DataFlowRunnerSample.java
@@ -7,24 +7,29 @@ package com.fluxtion.dataflow.reference.eventfeed;
 
 import com.fluxtion.dataflow.builder.DataFlowBuilder;
 import com.fluxtion.dataflow.runtime.DataFlow;
-import com.fluxtion.dataflow.runtime.eventfeed.DataFlowAgentRunner;
+import com.fluxtion.dataflow.runtime.eventfeed.DataFlowRunner;
+import com.fluxtion.dataflow.runtime.eventfeed.FileEventFeed;
+import com.fluxtion.dataflow.runtime.eventfeed.ReadStrategy;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class DataFlowRunnerSample {
     public static void main(String[] args) throws InterruptedException {
-        MyEventFeed myFeed = new MyEventFeed("myFeed");
+        System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+
+        FileEventFeed myFileFeed = new FileEventFeed(
+                "./data/fileFeedTest/input.txt",
+                "myFeed",
+                ReadStrategy.EARLIEST);
+
         DataFlow dataFlow = DataFlowBuilder.subscribeToFeed("myFeed", String.class)
-                .console("received {}")
+                .peek(s -> log.info("flow in:{}", s))
                 .build();
 
-        DataFlowAgentRunner runner = new DataFlowAgentRunner();
+        DataFlowRunner runner = new DataFlowRunner();
         runner.addDataFlow(dataFlow);
-        runner.addFeed(myFeed);
+        runner.addFeed(myFileFeed);
 
         runner.start();
-
-        for (int i = 0; i < 10; i++) {
-            Thread.sleep(1_000);
-            myFeed.publish("count-" + i);
-        }
     }
 }
