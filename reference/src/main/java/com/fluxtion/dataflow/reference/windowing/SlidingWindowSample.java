1661ec31b08cd84cae630fa4b97df893c9a3051f
greg higgins <greg.higgins@nonco.com>
2025-09-22 14:56:07 +0100

    Refactor `SlidingWindowSample` into

--- reference/src/main/java/com/fluxtion/dataflow/reference/windowing/SlidingWindowSample.java
+++ reference/src/main/java/com/fluxtion/dataflow/reference/windowing/SlidingWindowSample.java
@@ -3,25 +3,35 @@
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
+package com.fluxtion.dataflow.reference.windowing;
+
 import com.telamin.fluxtion.builder.DataFlowBuilder;
 import com.telamin.fluxtion.runtime.DataFlow;
 import com.telamin.fluxtion.runtime.flowfunction.helpers.Aggregates;
 import lombok.SneakyThrows;
 
-@SneakyThrows
-void main() {
-    DataFlow processor = DataFlowBuilder.subscribe(Integer.class)
-            .slidingAggregate(Aggregates.intSumFactory(), 300, 4)
-            .console("current sliding 1.2 second sum:{} timeDelta:%dt")
-            .build();
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-    Random rand = new Random();
+public class SlidingWindowSample {
 
-    try (ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()) {
-        executor.scheduleAtFixedRate(
-                () -> processor.onEvent(rand.nextInt(100)),
-                10, 10, TimeUnit.MILLISECONDS);
-        Thread.sleep(4_000);
+    @SneakyThrows
+    public static void main(String[] args) {
+        DataFlow processor = DataFlowBuilder.subscribe(Integer.class)
+                .slidingAggregate(Aggregates.intSumFactory(), 300, 4)
+                .console("current sliding 1.2 second sum:{} timeDelta:%dt")
+                .build();
+
+        Random rand = new Random();
+
+        try (ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleAtFixedRate(
+                    () -> processor.onEvent(rand.nextInt(100)),
+                    10, 10, TimeUnit.MILLISECONDS);
+            Thread.sleep(4_000);
+        }
     }
 }
 
